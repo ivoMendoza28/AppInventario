fcf66a510d02b28d034c7b2e25e44e07f46f8028
ivoMendoza28 <ivomendoza28@outlook.com>
2022-04-29 00:13:55 -0300

    Agregar archivos de proyecto.

---
+++ AppInventario/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppInventario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnprod_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Produccion());
+        }
+        private void btnpick_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AppInventario/Produccion.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppInventario
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Produccion : ContentPage
+    {
+        public Produccion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnreiniciar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnexport_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnimport_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
